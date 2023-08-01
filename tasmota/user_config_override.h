c797a2d7b06f4d6b9b08449f00ffede169f31440
dk9jh1970 <75255766+dk9jh1970@users.noreply.github.com>
2023-08-01 16:40:39 +0200

    Create user_config_override.h

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,16 @@
+// für Smart-Meter eBZ DD3 2R06 DTA-ODZ1
+
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+ 
+#ifndef USE_SCRIPT
+#define USE_SCRIPT
+#endif
+#ifndef USE_SML_M
+#define USE_SML_M
+#endif
+#ifdef USE_RULES
+#undef USE_RULES
+#endif
+ 
+#endif  // _USER_CONFIG_OVERRIDE_H_
